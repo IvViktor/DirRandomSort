52e66ef653a70f8c9880a7a4289324f709bd168c
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-02-01 11:59:58 +0200

    changed AppMain.java. Now app can copy file to other dir

--- src/main/java/ua/kiev/ivviktor/AppMain.java
+++ src/main/java/ua/kiev/ivviktor/AppMain.java
@@ -1,25 +1,25 @@
 package ua.kiev.ivviktor;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
 import java.util.Set;
-import java.util.Iterator;
 
 public class AppMain
 {
+	private static final String usage="USAGE:\r\nDirRandomOrder -s <directory> -t <directory>";
     public static void main( String[] args )
     {
-		if(args.length!=1){System.out.println("SYNTAX ERROR. YOU SHOULD PUT A NUMBER.");
-																		System.exit(1);}	
-
-		int bounds=Integer.parseInt(args[0]);	
-		Set<Integer> numberSet=new Randomizer().getUniqRandomsSet(bounds);
-		for(Iterator<Integer> iterator=numberSet.iterator();iterator.hasNext();){
-				System.out.print(iterator.next()+" ");
-		}
-		System.out.println();
+		if(args.length!=4){System.out.println("SYNTAX ERROR.\r\n"+usage);System.exit(1);}	
+    	File srcDir=null, targetDir=null;
+    	for(int i=0;i<args.length;i++){
+    		if(args[i]=="-s") srcDir=new File(args[++i]);
+    		else if(args[i]=="-t"){ targetDir=new File(args[++i]);targetDir.mkdir();}
+    		else{ System.out.println(usage);System.exit(1);}
+    	}
+		int filesNum=srcDir.listFiles().length;
+		Set<Integer> numberSet=new Randomizer().getUniqRandomsSet(filesNum);
+		DirCopy dc=new DirCopy(srcDir, targetDir);
+		dc.copyDir(numberSet);
+		System.out.println("Files successfully copied into "+targetDir.getName());
     }
 	
 	
